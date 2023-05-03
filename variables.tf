d3299b1a0b70fd9881bb1ca82f46c80e817eb5bd
Kashyap Rathore <32009249+Kashyap-Rathore@users.noreply.github.com>
2023-05-03 14:01:34 +0530

    adding files

---
+++ variables.tf
@@ -0,0 +1,24 @@
+variable "region" {
+  type    = string
+  default = "ap-south-1"
+}
+variable "subnet" {
+  type    = string
+  default = "subnet-07bf5a1f7c2ea45ed"
+}
+
+variable "aws_access_key" {
+  type    = string
+  default = "AKIAQUGTKA6KERANDING"
+}
+variable "aws_secret_access" {
+  type    = string
+  default = "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"
+}
+
+
+
+
+
+
+
